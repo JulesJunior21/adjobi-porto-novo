6d1563894b434c93b34a59dcaa1e229c2508156a
jules-tony <julesjunior.agb@gmail.com>
2025-05-08 11:53:11 +0200

    Ajout des pages "Événements", "Galerie", "Projets en cours", "Calendrier des assemblées", "Procès-verbaux", "S'engager", "Liste des commissions", "Rapports annuels", "Règlement intérieur" et "Statuts" avec en-têtes et pieds de page.

---
+++ src/app/assemblees/calendrier/page.jsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { Header } from '@/components/Header'
+import { Footer } from '@/components/Footer'
+import { Container } from '@/components/Container'
+
+export default function Calendrier() {
+  return (
+    <div className="bg-white">
+      <Header />
+      <main className="isolate">
+        {/* Hero section */}
+        <div className="relative isolate -z-10 overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-14">
+          <div
+            aria-hidden="true"
+            className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
+          />
+          <div className="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
+            <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-8 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
+              <h1 className="max-w-2xl text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl lg:col-span-2 xl:col-auto">
+                Calendrier des assemblées
+              </h1>
+            </div>
+          </div>
+          <div className="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32" />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  )
+}
